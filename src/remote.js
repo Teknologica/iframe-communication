07dba754ec7fbb816aea990ea2e3a95c7feec279
Andrei Moldoveanu <andrei@ilabsinc.com>
2018-06-05 16:13:26 -0400

    add controller

--- src/remote.js
+++ src/remote.js
@@ -1,19 +1,16 @@
 window.Foobar = function () {
-    const remoteFieldSrc = 'https://teknologica.github.io/iframe-communication/field.html';
+    const remoteFieldSrc = 'https://teknologica.github.io/iframe-communication/';
+
+    function getRemote(page) {
+        return remoteFieldSrc + page + '.html';
+    }
 
     const Element = function (target) {
         const parent = document.createElement('div');
-        const iframe = document.createElement('iframe');
-
+        parent.setAttribute('data-foobar-internal', target.getAttribute('data-foobar'));
+        const iframe = Element.createIframe(getRemote('field'));
         parent.className = 'foobar-outer-element';
         parent.setAttribute('data-foobar-internal', target.getAttribute('data-foobar'));
-        iframe.setAttribute('src', remoteFieldSrc);
-        iframe.setAttribute('allowtransparency', 'true');
-        iframe.setAttribute('border', '0');
-        iframe.setAttribute('frameborder', '0');
-        iframe.setAttribute('scrolling', 'no');
-        iframe.setAttribute('style', 'background: transparent; width: 100%; height: 100%;');
-
         target.appendChild(parent);
         parent.appendChild(iframe);
 
@@ -21,8 +18,27 @@ window.Foobar = function () {
         this.iframe = iframe;
     };
 
+    Element.createIframe = function(url, style, id) {
+        const iframe = document.createElement('iframe');
+        const appliedStyle = style || 'background: transparent; width: 100%; height: 100%;';
+        iframe.setAttribute('src', url);
+        iframe.setAttribute('allowtransparency', 'true');
+        iframe.setAttribute('border', '0');
+        iframe.setAttribute('frameborder', '0');
+        iframe.setAttribute('scrolling', 'no');
+        iframe.setAttribute('style', appliedStyle);
+
+        return iframe;
+    };
+
     const Foobar = function () {
         this.mounted = [];
+    };
+
+    Foobar.prototype.prepare = function () {
+        const controller = Element.createIframe(getRemote('link'), 'width:0;height:0');
+        document.body.appendChild(controller);
+        this.controller = controller
     };
 
     Foobar.prototype.mount = function (target) {
