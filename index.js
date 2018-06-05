67e56b4bd3ef6a04275dad1baef4e151783d4bec
Andrei Moldoveanu <andrei@ilabsinc.com>
2018-06-05 15:24:53 -0400

    update

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const serveStatic = require('serve-static');
 const port = 3000;
 const app = express();
 app.use(serveStatic(__dirname));
-app.listen(3000);
-console.log('now serving on port ' + 3000);
+app.listen(port);
+console.log('now serving on port ' + port);
